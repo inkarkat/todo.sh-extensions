6db5c707e1fbcabda002eb6799e9850f6893cd1f
Ingo Karkat <swdev@ingo-karkat.de>
2023-06-21 09:01:26 +0200

    ENH: Generalize todo count to all supported local locations
    Not just TODOs in Git repos, by leveraging "todo-local.sh location" as the source for the TODO_DIR.
    For efficiency, we store that value in _PS1TodoLocalTodoDir; the lookup is a bit more costly as we cannot simply leverage an existing variable (like _PS1GitRoot) any longer.
    The panel function is invoked from a subshell, so we cannot directly memoize the value there, but instead need to define a separate _PS1TodoLocal() event handler for _PS1OnChangeDirectory.

--- shell/promptPanel_i.sh
+++ shell/promptPanel_i.sh
@@ -2,8 +2,8 @@
 
 _PS1Panel_todo()
 {
-    [ -n "${_PS1GitRoot:-}" ] \
-	&& typeset localTodoFilespec="${_PS1GitRoot}/todo.txt" \
+    [ -n "${_PS1TodoLocalTodoDir:-}" ] \
+	&& typeset localTodoFilespec="${_PS1TodoLocalTodoDir}/todo.txt" \
 	&& [ -r "$localTodoFilespec" ] \
 	|| return
 
@@ -12,3 +12,14 @@ _PS1Panel_todo()
 
     printf 'TODO:%d\n' "$todoCount"
 }
+
+_PS1TodoLocal()
+{
+    < <(todo-local.sh location) IFS=$'\n' read -r _PS1TodoLocalTodoDir
+
+    [ -n "$_PS1TodoLocalTodoDir" ] \
+	&& export _PS1TodoLocalTodoDir \
+	|| unset _PS1TodoLocalTodoDir
+}
+
+commandSequenceMunge _PS1OnChangeDirectory _PS1TodoLocal
