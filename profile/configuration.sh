ec2aa7c0119bfec2d39e1d10553a84d9d8a48025
Ingo Karkat <swdev@ingo-karkat.de>
2026-09-24 14:55:33 +0200

    configuration: Robustness: Don't abort a checkout if the todo.sh command fails
    This is currently happening when I'm on the (outdated) master branch in todo.txt-cli while my todo.sh extensions require certain new functionality.
    
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- profile/configuration.sh
+++ profile/configuration.sh
@@ -1,4 +1,4 @@
 #!/bin/sh source-this-script
 
 # Show tasks for the current branch [+ submodule] after switching branches.
-export GIT_CHECKOUT_BRANCH_POST_COMMAND='TODOTXT_VERBOSE=0 todo-local.sh here -s 3'
+export GIT_CHECKOUT_BRANCH_POST_COMMAND='TODOTXT_VERBOSE=0 todo-local.sh here -s 3 || :'
